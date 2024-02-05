44f67f271d318f0bf464e7df3e1293f8ca2c5dc2
Giovanni Scalmati <scalmati.giovanni@gmail.com>
2024-02-05 16:57:12 +0100

    db lanciate migrazioni con tabella

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+
+<x-layout>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12 display-1 text-danger">
+            <h1>Il portale dei Videogame</h1>
+        </div>
+    </div>
+</div>
+
+
+
+
+</x-layout>
